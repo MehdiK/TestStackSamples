e1464dc07ea8d48f25e4649a9d877584d2f0446b
MehdiK <me@mehdi-khalili.com>
2013-09-19 13:52:17 +0330

    created samples project and added BDDfy in

---
+++ BDDfy/TestStack.BDDfy.Samples/Atm/HtmlReportConfig.cs
@@ -0,0 +1,48 @@
+﻿using TestStack.BDDfy.Processors.Reporters.Html;
+
+namespace TestStack.BDDfy.Samples.Atm
+{
+    /// <summary>
+    /// This overrides the default html report setting
+    /// </summary>
+    public class HtmlReportConfig : DefaultHtmlReportConfiguration
+    {
+        public override bool RunsOn(Core.Story story)
+        {
+            return story.MetaData.Type.Namespace != null && story.MetaData.Type.Namespace.EndsWith("Atm");
+        }
+
+        /// <summary>
+        /// Change the output file name
+        /// </summary>
+        public override string OutputFileName
+        {
+            get
+            {
+                return "ATM.html";
+            }
+        }
+
+        /// <summary>
+        /// Change the report header to your project
+        /// </summary>
+        public override string ReportHeader
+        {
+            get
+            {
+                return "ATM Solutions";
+            }
+        }
+
+        /// <summary>
+        /// Change the report description
+        /// </summary>
+        public override string ReportDescription
+        {
+            get
+            {
+                return "A reliable solution for your offline banking needs";
+            }
+        }
+    }
+}
